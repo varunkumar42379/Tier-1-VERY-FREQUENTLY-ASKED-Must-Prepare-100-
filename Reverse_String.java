b871bfce08083e1e145367f4a1622e2c33e85435
varunkumar42379 <varunkumar42379@gmail.com>
2026-08-27 15:42:26 +0530

    Update Reverse_String.java

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,1 +1,9 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+    }
+}
