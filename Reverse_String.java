df41bc5cae146f8c9cf891823a307492f2f5d9fc
varunkumar42379 <varunkumar42379@gmail.com>
2026-08-27 15:49:35 +0530

    Update Reverse_String.java

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,9 +1,16 @@
-
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Start small. Ship something.");
+        String str="automation";
+        String str1="";
+        //StringBuilder str1=new StringBuilder(str).reverse();
+        char[] ch=str.toCharArray();
+
+   for(int i=ch.length-1;i>=0;i--){
+     str1+=ch[i];
+   }
+        System.out.println(str1);
     }
 }
