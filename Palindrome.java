42462f79c5cf4bbea165a02ce7e443546dca7716
varunkumar42379 <varunkumar42379@gmail.com>
2026-08-27 15:51:52 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        String str="madam";
+        String str1="";
+        //StringBuilder str1=new StringBuilder(str).reverse();
+        char[] ch=str.toCharArray();
+
+   for(int i=ch.length-1;i>=0;i--){
+     str1+=ch[i];
+   }
+        System.out.println(str1.equals(str));
+    }
+}
